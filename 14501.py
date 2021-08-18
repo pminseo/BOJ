78ef09fbfaaaafb8e6865cdfb42b92604cc0f2ef
pminseo <83129701+pminseo@users.noreply.github.com>
2021-08-18 21:29:42 +0900

    Add files via upload

---
+++ 14501.py
@@ -0,0 +1,16 @@
+n = int(input())
+t = list()
+p = list()
+dp = list()
+for _ in range(n):
+    a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+    dp.append(b)
+dp.append(0)
+for i in range(n - 1, -1, -1):
+    if t[i] + i > n:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
+print(dp[0])
