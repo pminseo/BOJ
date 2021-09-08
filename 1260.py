0204e36d1175155d57852425fe47f7ae2a9e7e0a
pminseo <83129701+pminseo@users.noreply.github.com>
2021-09-08 19:12:49 +0900

    Add files via upload

--- 1260.py
+++ 1260.py
@@ -1,17 +1,40 @@
-n, m, v = map(int, input().split())
-graph = [[0]*(n+1)]*(n+1)
-visit_dfs = [0]*(n+1)
-order_dfs = []
+from collections import deque
+
+n, m, start = map(int, input().split())
+visited = [ False for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+    graph[a].append(b)
+    graph[b].append(a)
+for i in range(n+1):
+    if graph[i]:
+        graph[i].sort()
 
-def DFS(start):
-    visit_dfs[start] = 1
-    order_dfs.append(start)
-    for i in range(1, n+1):
-        if graph[start][i] == 1 and visit_dfs[i] == 0:
-            DFS(i)
-DFS(v)
-print(order_dfs)
+
+def dfs(graph, start, visited):
+    x = start
+    visited[x] = True
+    print(x, end=' ')
+    for i in graph[x]:
+        if i and visited[i] == False:
+            dfs(graph, i, visited)
+
+dfs(graph, start, visited)
+print()
+
+visited = [ False for _ in range(n+1)]
+q = deque()
+def bfs(graph, start, visited):
+    q.append(start)
+    visited[q[0]] = True
+    while q:
+        x = q.popleft()
+        print(x, end=' ')
+        for i in graph[x]:
+            if i and visited[i] == False:
+                q.append(i)
+                visited[i] = True
+
+bfs(graph, start, visited)
